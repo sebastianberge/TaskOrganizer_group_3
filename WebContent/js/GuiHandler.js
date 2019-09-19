0ec1c3689a1a879f7001351f4a1c43934f21770c
Anders Mæhlum Halvorsen <31239471+andersmh@users.noreply.github.com>
2019-09-19 13:38:04 +0200

    Changed to jsp and added the view

---
+++ WebContent/js/GuiHandler.js
@@ -0,0 +1,82 @@
+"use strict";
+{
+class GuiHandler{
+	constructor (allstatuses){
+		this.allstatuses = allstatuses
+	}
+	set allstatuses(allstatuses){
+		this._allstatuses = allstatuses
+	}
+	set deleteTaskCall(task){
+		
+	}
+	set deleteTaskCall(task){
+		
+	}
+	get allstatuses(){
+		return this._allstatuses
+	}
+	
+	showTask(task){
+		const tasksDiv = document.getElementById('tasks')
+		let stringOptions
+		for(status in this.allstatuses){
+			stringOptions += "<option value=\"" + this.allstatuses[status] +"\">" + this.allstatuses[status] + "</option>"
+			
+        }
+		if(tasksDiv.getElementsByTagName('table').length == 0){
+				console.log(tasksDiv.getElementsByTagName('table').length + "if")
+			
+			tasksDiv.innerHTML = "<table><thead><tr><th>Task</th><th>Status</th></tr></thead> <tbody><tr id=\"" + task.id + "\">"+
+                "<td>" + task.title + "</td>" +
+                "<td>" + task.status + "</td>" +
+                "<td>" +
+                    "<select>" +
+                       " <option value=\"0\" selected=\"\">&lt;Modify&gt;</option>" + stringOptions + 
+                    "</select>" +
+                "</td>" + 
+                "<td><button type=\"button\">Remove</button></td>" +
+           " </tr></tbody></table>"
+		}else{
+			const newTask = document.createElement("tr")
+			newTask.setAttribute("id", task.id)
+			newTask.innerHTML = "<td>" + task.title + "</td>" +
+            "<td>" + task.status + "</td>" +
+            "<td>" +
+                "<select>" +
+                   " <option value=\"0\" selected=\"\">&lt;Modify&gt;</option>" + stringOptions + 
+                "</select>" +
+            "</td>" + 
+            "<td><button type=\"button\">Remove</button></td>"
+            const list = tasksDiv
+			tasksDiv.getElementsByTagName('tbody')[0].appendChild(nyTask)
+		}
+	}
+	
+	updateTask(task){
+		
+	}
+	removeTask(id){
+		
+	}
+	noTask(){
+		
+	}
+}
+
+const gui = new GuiHandler()
+const statuses = ["WAITING","ACTIVE","DONE"]
+const tasks = [
+    {"id":1,"title":"Paint roof","status":"WAITING"},
+    {"id":2,"title":"Clean floor","status":"DONE"},
+    {"id":3,"title":"Wash windows","status":"ACTIVE"}
+]
+
+gui.allstatuses = statuses
+
+tasks.forEach((task) => {gui.showTask(task)})
+
+
+
+
+}
---
+++ WebContent/tasks.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript" src="js/GuiHandler.js" defer></script>
+<!--<script type="text/javascript" src="js/addTasks.js"></script> -->
+<meta charset="ISO-8859-1">
+<title>Task Organizer</title>
+</head>
+<body>
+	<div id="message">
+		<p>Waiting for server data.</p>
+	</div>
+	<div id="newtask">
+		<button type="button" disabled>New task</button>
+	</div>
+	<div id="tasks"></div>
+</body>
+</html>
