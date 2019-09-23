ab3ebdb10f2023407e17531e8de1f8e86dcd8a9e
571559 <31239471+andersmh@users.noreply.github.com>
2019-09-23 22:04:40 +0200

    🚧

--- WebContent/js/GuiHandler.js
+++ WebContent/js/GuiHandler.js
@@ -87,17 +87,14 @@ class GuiHandler{
 		
 	}
 
-}
-
-  const gui = new GuiHandler();
+}	
   const statuses = ["WAITING", "ACTIVE", "DONE"];
   const tasks = [
-    { id: 1, title: "Paint roof", status: "WAITING" },
-    { id: 2, title: "Clean floor", status: "DONE" },
-    { id: 3, title: "Wash windows", status: "ACTIVE" }
-  ];
-
-  gui.allstatuses = statuses;
+	    { id: 1, title: "Paint roof", status: "WAITING" },
+	    { id: 2, title: "Clean floor", status: "DONE" },
+	    { id: 3, title: "Wash windows", status: "ACTIVE" }
+	  ];
+  const gui = new GuiHandler(statuses);
 
   tasks.forEach(task => {
     gui.showTask(task);
