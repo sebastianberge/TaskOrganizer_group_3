754be5bced295469e315b8405d7fdc8bb9724d84
571559 <31239471+andersmh@users.noreply.github.com>
2019-09-23 21:45:55 +0200

    update task now works

--- WebContent/js/GuiHandler.js
+++ WebContent/js/GuiHandler.js
@@ -1,11 +1,12 @@
 "use strict";
 {
+	
 class GuiHandler{
 	constructor (allstatuses){
-		this.allstatuses = allstatuses
+		this.allstatuses = allstatuses;
 	}
 	set allstatuses(allstatuses){
-		this._allstatuses = allstatuses
+		this._allstatuses = allstatuses;
 	}
 	set deleteTaskCall(task){
 		
@@ -14,74 +15,92 @@ class GuiHandler{
 		
 	}
 	get allstatuses(){
-		return this._allstatuses
+		return this._allstatuses;
 	}
 	
 	showTask(task){
-		const tasksDiv = document.getElementById('tasks')
-		let stringOptions
+		const tasksDiv = document.getElementById('tasks');
+		let stringOptions;
 		for(status in this.allstatuses){
-			stringOptions += "<option value=\"" + this.allstatuses[status] +"\">" + this.allstatuses[status] + "</option>"
+			if(task.status == this.allstatuses[status]){
+				stringOptions += "<option value=\"" + this.allstatuses[status] +"\" disabled>" + this.allstatuses[status] + "</option>";
+			}else{
+				stringOptions += "<option value=\"" + this.allstatuses[status] +"\">" + this.allstatuses[status] + "</option>";
+			}
 			
         }
-		if(tasksDiv.getElementsByTagName('table').length == 0){
-				
-			var output = '';
-			for(var i in tasks){
-			//console.log(tasksDiv.getElementsByTagName('table').length + "if")
-			
-			output  += " <tbody><tr id=\"" + 
-				tasks[i].id + "\">"+
-                "<td>" + tasks[i].title + "</td>" +
-                "<td>" + tasks[i].status + "</td>" +
+		if(tasksDiv.getElementsByTagName('table').length == 0){			
+			tasksDiv.innerHTML = "<table><thead><tr><th>Task</th><th>Status</th></tr></thead> <tbody><tr id=\"" + task.id + "\">"+
+                "<td>" + task.title + "</td>" +
+                "<td>" + task.status + "</td>" +
                 "<td>" +
                     "<select>" +
                        " <option value=\"0\" selected=\"\">&lt;Modify&gt;</option>" + stringOptions + 
                     "</select>" +
                 "</td>" + 
-                "<td><button type=\"button\">Remove</button></td>" 	
-			}
-				tasksDiv.innerHTML = "<table><thead><tr><th>Task</th><th>Status</th></tr></thead> " + output + " </tr></tbody></table>";
+                "<td><button type=\"button\">Remove</button></td>" +
+           " </tr></tbody></table>";
 		}else{
-			const newTask = document.createElement("tr")
-			newTask.setAttribute("id", task.id)
-			newTask.innerHTML = "<td>" + task.title + "</td>" +
+			const nyTask = document.createElement("tr");
+			nyTask.setAttribute("id", task.id);
+			nyTask.innerHTML = "<td>" + task.title + "</td>" +
             "<td>" + task.status + "</td>" +
             "<td>" +
                 "<select>" +
                    " <option value=\"0\" selected=\"\">&lt;Modify&gt;</option>" + stringOptions + 
                 "</select>" +
             "</td>" + 
-            "<td><button type=\"button\">Remove</button></td>"
-            const list = tasksDiv
-			tasksDiv.getElementsByTagName('tbody')[0].appendChild(nyTask)
+            "<td><button type=\"button\">Remove</button></td>";
+            const list = tasksDiv;
+			tasksDiv.getElementsByTagName('tbody')[0].appendChild(nyTask);
 		}
+
+		const select = document.getElementById(task.id).getElementsByTagName('select')[0];
+		select.addEventListener('change', function(){
+			gui.updateTask(task);
+		})
+		
 	}
 	
 	updateTask(task){
+		const node = document.getElementById(task.id);
+		if(task.title != null){
+			node.getElementsByTagName('td')[0].innerHTML = task.title;
+		}
+		const select = document.getElementById(task.id).getElementsByTagName('select')[0];
+		const status = select.options[select.selectedIndex].value;
+		if(status != 0){
+			node.getElementsByTagName('td')[1].innerHTML = status;
+		}
+		node.getElementsByTagName('select')[0].selectedIndex = 0;
 		
 	}
-	removeTask(id){
-		
+	
+	removeTask(id) {
+	      let task = document.getElementById(id);
+	      if (task != null) {
+	        task.parentElement.removeChild(task);
+	      }
 	}
+	
 	noTask(){
 		
 	}
+
 }
 
-const gui = new GuiHandler()
-const statuses = ["WAITING","ACTIVE","DONE"]
-const tasks = [
-    {"id":1,"title":"Paint roof","status":"WAITING"},
-    {"id":2,"title":"Clean floor","status":"DONE"},
-    {"id":3,"title":"Wash windows","status":"ACTIVE"}
-]
+  const gui = new GuiHandler();
+  const statuses = ["WAITING", "ACTIVE", "DONE"];
+  const tasks = [
+    { id: 1, title: "Paint roof", status: "WAITING" },
+    { id: 2, title: "Clean floor", status: "DONE" },
+    { id: 3, title: "Wash windows", status: "ACTIVE" }
+  ];
 
-gui.allstatuses = statuses
-tasks.forEach((task) => {gui.showTask(task)})
+  gui.allstatuses = statuses;
 
-
-
-
+  tasks.forEach(task => {
+    gui.showTask(task);
+  });
 
 }
