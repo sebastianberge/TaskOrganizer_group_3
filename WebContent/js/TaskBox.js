bd917a6f8964b4dc0869b2ee3d27792135670d5b
Anders Mæhlum Halvorsen <31239471+andersmh@users.noreply.github.com>
2019-09-25 11:55:59 +0200

    TaskBox and tasks.jsp updated with testing for modal

--- WebContent/js/TaskBox.js
+++ WebContent/js/TaskBox.js
@@ -1,38 +1,74 @@
-class TaskBox{
-	constructor(allstatuses){
-		this.allstatuses = allstatuses;
-		this.onsubmit = null;
-	}
-	
-	/*
-	 * Setter/property to set a list of all possible task states, i.e. the
-	 * values that can be selected in the select element.
-	 */
-	set allstatuses(allstatuses){
-		this._allstatuses = allstatuses;
-	}
-	
-	/*
-	 * Setter/property to set a list of all possible task states, i.e. the
-	 * values that can be selected in the select element.
-	 */
-	get allstatuses(){
-		return this._allstatuses;
-	}
-	
-	/*
-	 * the modale box should disappear from view.
-	 */
-	close(){
-		 let taskBox = document.getElementById("taskbox");
-		 taskBox.style.display = "none";
-	}
-	
-	show(){
-	
-	}
-	
-	submit(){
-	
-	}
+"use strict";
+
+/*
+ * This class enables use of the addTask Modal, it shows a modal when
+ * initialized and called with show(). It has a callback function that will be
+ * used when a task is added. Set this like t.onsubmit = x
+ */
+
+class TaskBox {
+
+    constructor() {
+        this._allstatuses = [];
+        this._onsubmit = null;
+    }
+
+    get allstatuses() {
+        return this._allstatuses;
+    }
+
+    set allstatuses(s) {
+        this._allstatuses = s;
+    }
+
+    set onsubmit(o) {
+        this._onsubmit = o;
+    }
+
+    close() {
+        let m = document.getElementById("taskbox");
+        m.style.display = "none";
+    }
+
+    show() {
+        // Get the modal
+        let modal = document.getElementById("taskbox");
+
+        // Get the <span> element that closes the modal
+        let span = document.getElementsByClassName("close")[0];
+
+        let select = document.getElementById("modalStatuses");
+        if (!select.hasChildNodes()) {
+            for (let i = 0; i < this._allstatuses.length; i++) {
+                let el = document.createElement("option");
+                el.innerText = this._allstatuses[i];
+                select.appendChild(el);
+            }
+        }
+
+        // Show the modal.
+        modal.style.display = "block";
+
+        // When the user clicks on <span> (x), close the modal
+        span.addEventListener("click", () => this.close(), true);
+
+        let addTaskButton = document.getElementById("addTaskButton");
+
+        addTaskButton.addEventListener("click", () => this.submit(), true);
+    }
+
+    submit() {
+        let title = document.getElementById("taskInput").value;
+        let select = document.getElementById("modalStatuses");
+        let status = select.options[select.selectedIndex].value;
+        let task = new Task(title, status);
+        this._onsubmit(task)
+    }
+}
+
+class Task {
+    constructor(title, status) {
+        this.title = title;
+        this.status = status;
+    }
 }
--- WebContent/tasks.jsp
+++ WebContent/tasks.jsp
@@ -4,7 +4,9 @@
 <html>
 <head>
 <link href="css/style.css" rel="stylesheet" type="text/css">
-<script type="text/javascript" src="js/GuiHandler.js" defer></script>
+<script type="text/javascript" src="./js/GuiHandler.js" defer></script>
+<script type="text/javascript" src="./js/TaskBox.js" defer></script>
+
 <meta charset="ISO-8859-1">
 <title>Task Organizer</title>
 </head>
@@ -13,8 +15,31 @@
 		<p>Waiting for server data.</p>
 	</div>
 	<div id="newtask">
-		<button type="button" disabled>New task</button>
+		<button type="button">New task</button>
 	</div>
 	<div id="tasks"></div>
+
+	<!-- UNDER CONSTRUCTION -->
+	<!-- The Modal -->
+	<div class="modal" id="taskbox">
+		<!-- Modal content -->
+		<div class="modal-content">
+			<span class="close">&times;</span>
+			<table>
+				<tr>
+					<th>Title:</th>
+					<td><input id="taskInput" maxlength="80" size="25" type="text" /></td>
+				</tr>
+				<tr>
+					<th>Status:</th>
+					<td><select id="modalStatuses"></select></td>
+				</tr>
+			</table>
+			<p>
+				<button id="addTaskButton" type="submit">Add task</button>
+			</p>
+		</div>
+	</div>
+	<!-- UNDER CONSTRUCTION -->
 </body>
 </html>
