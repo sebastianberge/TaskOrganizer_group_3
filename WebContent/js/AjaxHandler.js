5845835cee7e024e370969060762e83bdd27002b
Anders Mæhlum Halvorsen <31239471+andersmh@users.noreply.github.com>
2019-09-27 16:24:27 +0200

    Added the test fetches from taskDemo to see if it worked (it did :100: )

--- WebContent/js/AjaxHandler.js
+++ WebContent/js/AjaxHandler.js
@@ -1,4 +1,4 @@
-async function main() {
+async function allstatuses() {
     const url='../TaskServices/broker/allstatuses'
 
     try {
@@ -14,4 +14,77 @@ async function main() {
         }
 }
 
-main()
+async function deleteTask() {
+    const url='../TaskServices/broker/task/2'
+    try {
+        const response = await fetch(url,{method: "DELETE"})
+        try {
+            const text = await response.text()
+            console.log(text)
+        } catch (error) {
+            console.log(error)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function modifyTask() {
+    const url='../TaskServices/broker/task/2'
+    try {
+        const response = await fetch(url,{
+            method: "PUT",
+            headers: {"Content-Type": "application/json; charset=utf-8"},
+            body: JSON.stringify({'status': 'DONE'})
+        })
+        try {
+            const text = await response.text()
+            console.log(text)
+        } catch (error) {
+            console.log(error)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function newTask() {
+    
+    const url='../TaskServices/broker/task'
+    try {
+        const response = await fetch(url,{
+            method: "POST",
+            headers: {"Content-Type": "application/json; charset=utf-8"},
+            body: JSON.stringify({"title":"Something more to do", "status": "WAITING"})
+        })
+        try {
+            const text = await response.text()
+            console.log(text)
+        } catch (error) {
+            console.log(error)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function taskList() {
+    const url='../TaskServices/broker/tasklist'
+    try {
+        const response = await fetch(url,{method: "GET"})
+        try {
+            const text = await response.text()
+            console.log(text)
+        } catch (error) {
+            console.log(error)
+        }            
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+allstatuses()
+deleteTask()
+modifyTask()
+newTask()
+taskList()
