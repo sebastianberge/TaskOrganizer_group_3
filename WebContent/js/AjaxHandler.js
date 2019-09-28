0c43f9f4819ed04262ebd0105ffc33a74c3fde96
Anders Mæhlum Halvorsen <31239471+andersmh@users.noreply.github.com>
2019-09-28 13:12:42 +0200

    AjaxHandler complete

--- WebContent/js/AjaxHandler.js
+++ WebContent/js/AjaxHandler.js
@@ -1,90 +1,90 @@
-async function allstatuses() {
-    const url='../TaskServices/broker/allstatuses'
+"use strict";
 
-    try {
-        const response = await fetch(url,{method: "GET"})
-        try {
-            const text = await response.text()
-            console.log(text)
-        } catch (error) {
-            console.log(error)
-            }
-        } catch (error) {
-            console.log(error)
-        }
+class AjaxHandler {
+	
+	async allstatuses() {
+	    const url='../TaskServices/broker/allstatuses'
+
+	    try {
+	        const response = await fetch(url,{method: "GET"})
+	           
+	            try{
+	            	const text = await response.text()
+	            	return text
+	            } catch (error){
+	            	console.log(error)
+	            }
+	       	} catch (error) {
+	            console.log(error)
+	        }  
+	}
+	
+	async deleteTask(id) {
+	    const url='../TaskServices/broker/task/' + id
+	    try {
+	        const response = await fetch(url,{method: "DELETE"})
+	        try {
+	            const text = await response.text()
+	            return text
+	        } catch (error) {
+	            console.log(error)
+	        }
+	    } catch (error) {
+	        console.log(error)
+	    }
+	}
+	
+	async modifyStatus(task) {
+	    const url='../TaskServices/broker/task/' + task.id 
+	    try {
+	        const response = await fetch(url,{
+	            method: "PUT",
+	            headers: {"Content-Type": "application/json; charset=utf-8"},
+	            body: JSON.stringify({'status': task.status})
+	        })
+	        try {
+	            const text = await response.text()
+	            return text
+	        } catch (error) {
+	            console.log(error)
+	        }
+	    } catch (error) {
+	        console.log(error)
+	    }
+	}
+	
+	async addNewTask(task) {
+	    const url='../TaskServices/broker/task'
+	    try {
+	        const response = await fetch(url,{
+	            method: "POST",
+	            headers: {"Content-Type": "application/json; charset=utf-8"},
+	            body: JSON.stringify({"title":task.title, "status": task.status})
+	        })
+	        try {
+	            const text = await response.text()
+	            return text
+	        } catch (error) {
+	            console.log(error)
+	        }
+	    } catch (error) {
+	        console.log(error)
+	    }
+	}
+	
+	async getAllTasks() {
+	    const url='../TaskServices/broker/tasklist'
+	    try {
+	        const response = await fetch(url,{method: "GET"})
+	        try {
+	            const tasks = await response.text()
+	            console.log(tasks)
+	            return tasks
+	        } catch (error) {
+	            console.log(error)
+	        }            
+	    } catch (error) {
+	        console.log(error)
+	    }
+	}
 }
-
-async function deleteTask() {
-    const url='../TaskServices/broker/task/2'
-    try {
-        const response = await fetch(url,{method: "DELETE"})
-        try {
-            const text = await response.text()
-            console.log(text)
-        } catch (error) {
-            console.log(error)
-        }
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-async function modifyTask() {
-    const url='../TaskServices/broker/task/2'
-    try {
-        const response = await fetch(url,{
-            method: "PUT",
-            headers: {"Content-Type": "application/json; charset=utf-8"},
-            body: JSON.stringify({'status': 'DONE'})
-        })
-        try {
-            const text = await response.text()
-            console.log(text)
-        } catch (error) {
-            console.log(error)
-        }
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-async function newTask() {
-    
-    const url='../TaskServices/broker/task'
-    try {
-        const response = await fetch(url,{
-            method: "POST",
-            headers: {"Content-Type": "application/json; charset=utf-8"},
-            body: JSON.stringify({"title":"Something more to do", "status": "WAITING"})
-        })
-        try {
-            const text = await response.text()
-            console.log(text)
-        } catch (error) {
-            console.log(error)
-        }
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-async function taskList() {
-    const url='../TaskServices/broker/tasklist'
-    try {
-        const response = await fetch(url,{method: "GET"})
-        try {
-            const text = await response.text()
-            console.log(text)
-        } catch (error) {
-            console.log(error)
-        }            
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-allstatuses()
-deleteTask()
-modifyTask()
-newTask()
-taskList()
